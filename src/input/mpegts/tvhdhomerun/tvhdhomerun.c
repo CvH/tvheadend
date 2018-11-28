5787e0f8fcd39de1a86a5c5d5da9dc35cbbf92c3
CvH <1355173+CvH@users.noreply.github.com>
2018-11-28 17:06:42 +0100

    Update tvhdhomerun.c

--- src/input/mpegts/tvhdhomerun/tvhdhomerun.c
+++ src/input/mpegts/tvhdhomerun/tvhdhomerun.c
@@ -17,7 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "libhdhomerun/hdhomerun.h"
+#include "hdhomerun.h"
 
 #include "tvheadend.h"
 #include "input.h"
